32292ce4063d2df5531ae5e943a02f190c6fdff5
Alessio Valdarchi <Valdanito994@gmail.com>
2022-08-25 13:11:04 +0200

    fix es02

--- exercises/src/index.js
+++ exercises/src/index.js
@@ -1,4 +1,6 @@
-class HelloWorld extends React.Component {
+import React from 'react';
+
+export class HelloWorld extends React.Component {
     render() {
         return <h1>Hello, World</h1>
     }
