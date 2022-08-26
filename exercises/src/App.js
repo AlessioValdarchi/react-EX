9bd12d8bb5db5ea113c61217f7d32673c8d08f19
Alessio Valdarchi <Valdanito994@gmail.com>
2022-08-26 16:10:32 +0200

    ex04 done

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,7 +1,7 @@
 import { Hello } from "./Hello";
 import React from "react";
 
-class App extends React.Component {
+export class App extends React.Component {
     render() {
         return (<div>
             <Hello />
