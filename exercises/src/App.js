29e91fe1c50e155c4d4f611e0339ffeefa0f4b19
Alessio Valdarchi <Valdanito994@gmail.com>
2022-08-28 11:27:54 +0200

    prop01 done

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,10 +1,12 @@
 import { Hello } from "./Hello";
 import React from "react";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
     render() {
         return (<div>
             <Hello />
+            <Welcome name='Alex' />
         </div>
         )
     }
