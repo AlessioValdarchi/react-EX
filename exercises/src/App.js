bc5e074cbfaf52fcacb002c005a802559a852aa8
Alessio Valdarchi <Valdanito994@gmail.com>
2022-08-30 13:12:46 +0200

    state01 done

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,12 +1,14 @@
 import { Hello } from "./Hello";
 import React from "react";
 import { Welcome, name } from "./Welcome";
+import { Counter } from "./Counter";
 
 export class App extends React.Component {
     render() {
         return (<div>
             <Hello />
             <Welcome name={name} age={10} />
+            <Counter />
         </div>
         )
     }
