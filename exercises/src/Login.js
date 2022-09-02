e96ca43db98ca20f3c4d42fcc09ec005de462bf2
Alessio Valdarchi <Valdanito994@gmail.com>
2022-09-02 10:15:49 +0200

    form03 done

--- exercises/src/Login.js
+++ exercises/src/Login.js
@@ -4,7 +4,9 @@ export class Login extends React.Component {
     state = {
         username: '',
         password: '',
-        checkbox: false
+        checkbox: false,
+        disabled: true
+
     }
     inputOnChange = (event) => {
         const value = event.target.value;
@@ -18,14 +20,29 @@ export class Login extends React.Component {
 
     }
 
+    setDisabledButton = () => {
+        if (this.state.username && this.state.password && this.state.disabled) {
+            //enable it
+            this.setState({
+                disabled: false,
+            })
+        } else if ((!this.state.username || !this.state.password) && !this.state.disabled) {
+            //disable it
+            this.setState({
+                disabled: true,
+            })
+        }
+    }
+
 
     render() {
+        this.setDisabledButton();
         return <div>
             <h1>Form Login</h1>
             <input name="username" value={this.state.username} onChange={this.inputOnChange} />
             <input name="password" type='password' value={this.state.password} onChange={this.inputOnChange} />
             <input name="checkbox" type='checkbox' checked={this.state.checkbox} onChange={this.inputOnChange} />
-
+            <button name="login" disabled={this.state.disabled} onClick={() => this.props.onLogin(this.state)}>Login</button>
         </div>
     }
 }
