eecd2cb65954396b6c541a0683fee0f69fe4c729
Alessio Valdarchi <Valdanito994@gmail.com>
2022-09-05 12:42:24 +0200

    list06 done

--- exercises/src/TodoList.js
+++ exercises/src/TodoList.js
@@ -32,10 +32,20 @@ export class TodoList extends React.Component {
 
         })
     }
+    deleteItem = (e) => {
+        e.preventDefault()
+        this.setState({
+            items: this.state.items.filter((item, index) => item + index !== e.target.id),
+        },
+        )
+    }
+
     render() {
         return (
             <><ul>
-                {this.state.items.map((item, index) => <li key={item + index}>{item}</li>)}
+                {this.state.items.map((item, index) => <li key={item + index}>{item}
+                    <span><button id={item + index} onClick={this.deleteItem}>Delete item</button></span></li>
+                )}
             </ul>
                 <input onChange={this.setItem} name="text" value={this.state.currItem} />
                 <button onClick={this.addItem}>Add Item</button>
