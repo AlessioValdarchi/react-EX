40c55f76a960c61b365782fbc3b3ab6f721a6fcb
Alessio Valdarchi <Valdanito994@gmail.com>
2022-09-16 10:37:23 +0200

    ReactRouter-02

--- exercises/src/AppRout.js
+++ exercises/src/AppRout.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from './Welcome'
+import { Counter } from './Counter'
 export function AppRout() {
     return (
         <Routes>
             <Route path="/" element={<Welcome name={'Alessio'} />} />
+            <Route path="/counter" element={<Counter />} />
         </Routes>
     )
 }
