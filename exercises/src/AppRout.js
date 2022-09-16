f6a01c9086610eea4ef5cc83fc2fb464d47efd9c
Alessio Valdarchi <Valdanito994@gmail.com>
2022-09-16 11:10:42 +0200

    reactRout04 done

--- exercises/src/AppRout.js
+++ exercises/src/AppRout.js
@@ -1,13 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import { Welcome } from './Welcome'
 import { Counter } from './Counter'
 import { RootGitHubUser } from './RootGitHubUser'
 export function AppRout() {
-    return (
+    return (<div>
+        <h3>Navigate to:</h3>
+        <p><Link to={"/"}>Home</Link> | <Link to={"counter"}>Counter</Link> | <Link to={"/AlessioValdarchi"}>My Github</Link></p>
         <Routes>
             <Route path="/" element={<Welcome name={'Alessio'} />} />
             <Route path="/counter" element={<Counter />} />
             <Route path="/:username" element={<RootGitHubUser />} />
         </Routes>
+    </div>
     )
 }
