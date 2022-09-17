28857399b53cfbaf11a6311c0689b16fa63f254b
Alessio Valdarchi <Valdanito994@gmail.com>
2022-09-17 11:04:24 +0200

    swr02 done

--- exercises/src/useGitHubUser.js
+++ exercises/src/useGitHubUser.js
@@ -1,7 +1,7 @@
 import useSWR from "swr"
 const fetcher = fetch(url).then(response => response.json)
 export function useGitHubUser(username) {
-    const { data, error, loading = null, } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    const { data, error, loading = null, } = useSWR(() => !username ? null : `https://api.github.com/users/${username}`, fetcher)
 
     return { data, error, loading }
 }
